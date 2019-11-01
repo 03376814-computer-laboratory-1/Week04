be5c923380b57d2352fb0ccf84e1a8f019af3463
maethapon1999 <61030045@kmitl.ac.th>
2019-11-01 15:48:52 +0700

    add The value: 500.

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -7,7 +7,7 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("|{0, -10}|", 000);
+            Console.WriteLine("The value: {0}.", 500);
 
 
 
