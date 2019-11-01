29d55ce7b31776a5fba83bfd7af5790f4710791a
maethapon1999 <61030045@kmitl.ac.th>
2019-11-01 17:48:14 +0700

    add ฿12.35

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -7,7 +7,7 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The value: {0}.", 500);
+            Console.WriteLine("{0,-10:C}", 12.3456789);
 
 
 
