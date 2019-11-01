10b5166e96efd618d12c7bc8318fd21c73cf30ed
maethapon1999 <61030045@kmitl.ac.th>
2019-11-01 15:44:26 +0700

    add Here Come Are Slash \

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -7,7 +7,7 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello" + "World");
+            Console.WriteLine("Here Come Are Slash \\");
 
 
 
