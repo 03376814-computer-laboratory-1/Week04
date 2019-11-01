7da36ee07946955dd868c0c42f8a0a3a7c9ffb7f
maethapon1999 <61030045@kmitl.ac.th>
2019-11-01 15:42:49 +0700

    Add HelloWorld

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -7,7 +7,7 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{2:d} {0:d} {1:d}", 1, 2, 3);
+            Console.WriteLine("Hello" + "World");
 
 
 
