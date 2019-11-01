3d2f29b1278a6ea2b8f9859c9f436d0aa72e66d5
maethapon1999 <61030045@kmitl.ac.th>
2019-11-01 15:41:40 +0700

    Add 3 1 2

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -7,11 +7,9 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            string name = "Hello";
-            Console.WriteLine(String.Format("{0} there.I said {0}! {0}???", name));
+            Console.WriteLine("{2:d} {0:d} {1:d}", 1, 2, 3);
 
-            
-            
+
 
         }
     }
