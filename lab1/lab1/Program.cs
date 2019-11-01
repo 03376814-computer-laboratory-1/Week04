6f48a3ea03a2f809f41fabc42163bd577cd0832b
maethapon1999 <61030045@kmitl.ac.th>
2019-11-01 15:04:13 +0700

    add HelloHello

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -7,7 +7,8 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.Write("Hello");
+            Console.Write("Hello");
         }
     }
 }
