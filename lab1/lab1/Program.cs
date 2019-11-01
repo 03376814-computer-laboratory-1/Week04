48483b1c7d13e8fcdf450b44c199d9fcdf18d6d2
maethapon1999 <61030045@kmitl.ac.th>
2019-11-01 15:38:17 +0700

    Add Hello there.I said Hello! Hello???

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -7,12 +7,9 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            const double i = 123.456789d;
-            Console.WriteLine("{0:F1}", i);
-            Console.WriteLine("{0:F2}", i);
-            Console.WriteLine("{0:F3}", i);
-            Console.WriteLine("{0:F4}", i);
-            Console.WriteLine("{0:F5}", i);
+            string name = "Hello";
+            Console.WriteLine(String.Format("{0} there.I said {0}! {0}???", name));
+
             
             
 
