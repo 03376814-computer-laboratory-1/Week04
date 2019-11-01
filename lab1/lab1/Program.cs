23c8f84d1cc3f6d4aa484ce09acd043564d14a70
maethapon1999 <61030045@kmitl.ac.th>
2019-11-01 18:32:39 +0700

    add number

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -8,9 +8,9 @@ namespace lab1
         static void Main(string[] args)
         {
             string str;
-            Console.Write("Enter some characters.");
-            str = Console.ReadLine();
-            Console.WriteLine("You entered: " + str);
+            Console.Write("Please enter value 1 : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val1);
 
 
         }
