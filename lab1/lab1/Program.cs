31660542abdb5c8b296b3a92120f22da1c5f47ac
maethapon1999 <61030045@kmitl.ac.th>
2019-11-04 15:25:51 +0700

    add short

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -8,10 +8,10 @@ namespace lab1
         static void Main(string[] args)
         {
             {
-                Console.WriteLine("Data type : byte");
-                Console.WriteLine("Size :" + sizeof(byte));
-                Console.WriteLine("Minimum Value :" + byte.MinValue);
-                Console.WriteLine("Maximum Value :" + byte.MaxValue);
+                Console.WriteLine("Data type : short");
+                Console.WriteLine("Size :" + sizeof(short));
+                Console.WriteLine("Minimum Value :" + short.MinValue);
+                Console.WriteLine("Maximum Value :" + short.MaxValue);
             }
 
         }
