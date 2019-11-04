7cbbdac3beacde2ae6ea78afd16a18a3bdcf6392
maethapon1999 <61030045@kmitl.ac.th>
2019-11-04 15:18:29 +0700

    add Data type : int

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -7,16 +7,12 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            string n, s;
-            double id, gpa;
-            Console.Write("Name : ");
-            n = Console.ReadLine().ToString().ToUpper();
-            Console.WriteLine("Last Name : ");
-            s = Console.ReadLine().ToString().ToUpper();
-            Console.WriteLine("ID : ");
-            id = double.Parse(Console.ReadLine());
-            Console.WriteLine("GPA : ");
-            gpa = double.Parse(Console.ReadLine());
+            {
+                Console.WriteLine("Data type : int");
+                Console.WriteLine("Size :" + sizeof(int));
+                Console.WriteLine("Minimum Value :" + int.MinValue);
+                Console.WriteLine("Maximum Value :" + int.MaxValue);
+            }
 
         }
     }
